f1167cffc42df0e5f825e100293f30593924608b
Emanuel Lindstrom <emanuel.lindstrom@gmail.com>
2019-02-02 21:41:47 +0100

    Added XOR data set. Added comments to look for error and found that weights aren't updating correctly.

--- first_nn/nn1.py
+++ first_nn/nn1.py
@@ -3,10 +3,19 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
+def generate_set0_data(input_dim, p, noise_rate):
+    # Generate an XOR data set.
+    inputs_set0 = [[0,1], [0,0], [1,0], [1,1]]
+    labels_set0 = np.ones(len(inputs_set0))
 
+    for index, inputs in enumerate(inputs_set0):
+        if inputs[0] == 0:
+            labels_set0[index] = -1
+
+    return inputs_set0, labels_set0
 
 def generate_set1_data(input_dim, p, noise_rate):
-    # Generate random points between -1 and 1
+    # GEnerate a set of points and assign them to red category if they are within 0.2 units from a -45 degree line in the middle of the set
     inputs_set1 = np.random.rand(p, input_dim)*2-1
 
     n = (np.array([0.5, 0.5]) , np.array([0.5, 0.5]))
@@ -22,6 +31,7 @@ def generate_set1_data(input_dim, p, noise_rate):
     return inputs_set1, labels_set1
 
 def generate_set2_data(input_dim, p, noise_rate):
+    # Data set split into four quadrants where on category belong to quadrant 1 and 3 and the other to 2 and 4.
     inputs_set2 = np.random.rand(p,input_dim)*2-1
     labels_set2 = np.ones(p)*(-1)
 
@@ -29,6 +39,17 @@ def generate_set2_data(input_dim, p, noise_rate):
         if state[0] * state[1] > 0 or np.random.rand() < noise_rate:
             labels_set2[index] = 1
     return inputs_set2, labels_set2
+
+def generate_set3_data(input_dim, p, noise_rate):
+    inputs_set3 = np.random.rand(p, input_dim)*2-1
+    labels_set3 = np.ones(p)*(-1)
+
+    for index, state in enumerate(inputs_set3):
+        # if the point is within a circle at the origin with radius r, assign it label 1
+        r = 0.8
+        if np.sqrt(state[0]**2 + state[1]**2 ) < r or np.random.rand() < noise_rate:
+            labels_set3[index] = 1
+    return inputs_set3, labels_set3
 
 
 def create_weights(layers_dims, mean, std):
@@ -106,18 +127,22 @@ def NN(inputs, labels, weights, biases, layers_dims):
     
     # Enegy function
     H = np.sum((labels - Out)**2)/(2*p)
-    errors = p
+    correct_predictions = 0
     
     for index, label in enumerate(labels):
         if label == predicted_labels[index]:
-            errors -=1
-    accuracy = 1 - errors/p
+            correct_predictions +=1
+    
+    accuracy = correct_predictions/p
+   
+
 
     # calculate the first error delta^{(L)}
     output_state = [row[-1][0] for row in neuron_states]
 
 
     delta_out = g_prim(np.array(output_state)) * (labels - Out)
+    
 
     # Update the output node's bias
     biases[-1] = biases[-1] - eta * np.sum(delta_out) / p
@@ -126,18 +151,27 @@ def NN(inputs, labels, weights, biases, layers_dims):
 
     delta_hidden_layers = []
     old_delta = delta_out
+    # Calculate delta for each layer
+    input(f"Delta0 is {delta_out.shape}")
     for layer, _ in enumerate(hidden_layers_dims, 2):
-        layer = layer*(-1)
+        layer = layer*(-1) # Negative index to iterate backwards through the layers
         new_delta = []
+        print(f"Our layers are {hidden_layers_dims}, Active layer: {layer}")
+        print(F"Weights shape: {weights}")
+        print(f"Neuron states: {neuron_states[0]}")
+        print(f"Active neurons: {neuron_states[0][layer]}")
+        print(f"Active weights: {weights[layer+1][0]}")
+        print(f"Active old_delta: {old_delta[0]}")
+        input(f"...")
         for index in range(p):        
             new_delta.append( old_delta[index] * np.matmul(weights[layer+1][0], g_prim(neuron_states[index][layer])))
+            
         
         # Insert to beginning of list to keep weight ordered from left to right of the neural network
         delta_hidden_layers.insert(0, new_delta)
         old_delta = new_delta
 
     #update weights
-
     for index, delta in enumerate(delta_hidden_layers):
         biases[index] = biases[index] - eta * np.sum(delta) / p
 
@@ -153,14 +187,15 @@ def NN(inputs, labels, weights, biases, layers_dims):
 p = 500
 mean = 0
 std = 0.5
-batching = True
-epochs = 500
-eons = 10
+batching = 0
+epochs = 1
+eons = 1
 noise_rate = 0
 eta = 0.5
 input_dim = 2
 output_dim = 1
-hidden_layers_dims = [2]
+axis = [-1, 1]
+hidden_layers_dims = [2, 3, 2]
 layers_dims = [input_dim, *hidden_layers_dims, output_dim]  # Input, hidden and output layer dimensions
 
 
@@ -174,9 +209,12 @@ biases = create_biases([*hidden_layers_dims, output_dim])
 energy = np.zeros(epochs)
 accuracy = np.zeros(epochs)
 best_acc = 0
+lowest_energy = 9999
 plot_data = 1
+plot_best_data = 0
 best_epochs = np.zeros(eons)
 best_epochs_acc = np.zeros(eons)
+starting_weights = [2]
 
 for eon in range(eons):
     print(f"Eon no: {eon}")
@@ -184,6 +222,8 @@ for eon in range(eons):
     weights = create_weights(layers_dims, mean, std)
     biases = create_biases([*hidden_layers_dims, output_dim])
     best_acc_epoch = 0
+    lowest_energy_epoch = 0
+    starting_weights.append(weights)
     for epoch in range(epochs):
         if epoch%100==0:
             print(f"Epoch no: {epoch}")
@@ -210,16 +250,20 @@ for eon in range(eons):
                 batch_labels = np.array([row[1] for row in batch])
                 predictions_, weights, biases, H_, acc_ = NN(batch_inputs, batch_labels, weights, biases, layers_dims)
     
-        predictions, weights_, biases_, H, acc = NN(inputs, labels, weights, biases, layers_dims)
+        predictions, weights, biases, H, acc = NN(inputs, labels, weights, biases, layers_dims)
         energy[epoch] = H
         accuracy[epoch] = acc
         
         if acc > best_acc_epoch:
+        #if H < lowest_energy_epoch:
+            lowest_energy_epoch = H
             best_acc_epoch = acc
             best_epochs[eon] = epoch
             best_epochs_acc[eon] = acc
 
         if acc > best_acc:
+        #if H < lowest_energy:
+            lowest_energy = H
             best_acc = acc
             best_biases = biases
             best_weights = weights
@@ -228,10 +272,14 @@ for eon in range(eons):
             best_epoch = epoch
             print(f"Best epoch: {best_epoch}")
             print(f"Best eon: {best_eon}")
+            print(f"Accuracy {acc} | Energy {H}")
 
 print(f"Best epochs: {best_epochs}")
 print(f"Best ep acc: {best_epochs_acc}")
 print(f"Best accuracy of {best_acc} found in eon {best_eon} in epoch {best_epoch}")
+
+for index in range(eons):
+    print(f"Eon {index} | Epoch {best_epochs[index]} | Accuracy {best_epochs_acc[index]}")  #| Weights {[w[:] for w in starting_weights[index]]}")
 if plot_data:
     for index, state in enumerate(inputs):
         if labels[index] == 1:
@@ -240,11 +288,15 @@ if plot_data:
             plt.plot(state[0], state[1], 'b.')
         if predictions[index] == 1:
             plt.plot(state[0], state[1], 'ko', mfc='none')
-        x1_span = np.linspace(-1,1,100)
-    for idx, w in enumerate(best_weights[0]):
-        x2_span = (-w[0] * x1_span + best_biases[0][idx])/w[1]
+        x1_span = np.linspace(axis[0], axis[1],100)
+    for idx, w in enumerate(weights[0]):
+        x2_span = (-w[0] * x1_span + biases[0][idx])/w[1]
         plt.plot(x1_span, x2_span, "k")
         print(f"Slope gradient {idx}: {-w[0]/w[1]}")
+        print(f"Crossing y-axis at {biases[0][idx]/w[1]}")
+    #plot y-axis
+    plt.plot([0]*100, x1_span, 'k--')
+    plt.axis([*axis, *axis])
     plt.show()
 i = list(range(epochs))
 
@@ -256,10 +308,10 @@ i = list(range(epochs))
 #plt.show()
 
 plt.figure()
-plt.plot(i, accuracy, 'b.')
+plt.plot(i, accuracy, 'b-')
 plt.subplot(1,1,1)
 
-plt.plot(i, energy, 'r.')
+plt.plot(i, energy, 'r-')
 #plt.subplot(1,1,2)
 plt.show()
 
@@ -267,7 +319,7 @@ print(f"weights: {weights}")
 print(f"biases: {biases}")
 
 #Plot the data for best predicted labels
-if plot_data:
+if plot_best_data:
     for index, state in enumerate(inputs):
         if labels[index] == 1:
             plt.plot(state[0], state[1], 'r.')
@@ -276,9 +328,11 @@ if plot_data:
 
         if best_predictions[index] == 1:
             plt.plot(state[0], state[1], 'ko', mfc='none')
-        x1_span = np.linspace(-1,1,100)
+        x1_span = np.linspace(axis[0],axis[1],100)
     for idx, w in enumerate(best_weights[0]):
         x2_span = (-w[0] * x1_span + best_biases[0][idx])/w[1]
         plt.plot(x1_span, x2_span, "k")
         print(f"Slope gradient {idx}: {-w[0]/w[1]} with bias {best_biases[0][idx]}")
+        print(f"Crossing y-axis at {best_biases[0][idx]/w[1]}")
+    plt.axis([*axis, *axis])
     plt.show()
